6b3b3d50cb3a4a5008d465e56bbd8bb9e579a8bb
Alimpos <alimpostea@mail.ru>
2025-06-30 19:20:12 +0300

    added data

---
+++ src/shared/data/collumns.ts
@@ -0,0 +1,17 @@
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
