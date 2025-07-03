7d26bf29b891d463f628219cfbfe9a9ccf6f610c
Alimpos <alimpostea@mail.ru>
2025-07-03 21:47:44 +0300

    build

--- src/utils/userUtils.ts
+++
@@ -1,6 +0,0 @@
-import type { IUser } from '../shared/models/user';
-
-export function filterOutUser(users: IUser[] | null, userToDelete: IUser): IUser[] | null {
-    if (!users) return null;
-    return users.filter(u => u.key !== userToDelete.key);
-}
